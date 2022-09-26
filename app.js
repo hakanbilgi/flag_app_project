d70af91c1bb223813ce5942c8ec2c65e83763a25
Hakan Bilgi <hknblg34@gmail.com>
2022-09-26 16:46:57 +0300

    ...

--- app.js
+++ app.js
@@ -1,6 +1,50 @@
-//*=========================================================
-//*                     FLAG-APP
-//*=========================================================
+
+const select = document.getElementById("select")
+
+const fetchCountryByAllName = async () => {
+  const url = "https://restcountries.com/v3.1/all";
+  fetch(url)
+    .then((res) => {
+      if (!res.ok) {
+        renderError(`Something went wrong: ${res.status}`);
+        throw new Error();
+      }
+      return res.json();
+    })
+    .then((data) => renderNames(data))
+    .catch((err) => console.log(err));
+};
+
+
+let isError = false;
+const renderNames = (data)=> {
+  const select = document.getElementById("select");
+  if(isError) {
+    document.querySelector("body") += `
+      <h2>News Can not be fetched</h2>
+      <img src="./img/404.png" alt="" />
+    `;
+    return;
+  }
+  let names = data.map((data) => data.name.common).sort();
+  names.forEach((item)=> {
+    
+    select.innerHTML += `
+    <option value="${item}">${item}</option>
+    `;
+
+  })
+  console.log(data)
+
+}
+
+select.addEventListener("change",(e)=> {
+
+  console.log( e.target.value)
+  fetchCountryByName(e.target.value)
+})
+
+
 
 const fetchCountryByName = (name) => {
   const url = `https://restcountries.com/v3.1/name/${name}`;
@@ -34,13 +78,14 @@ const renderCountries = (data) => {
     languages,
     name: { common },
     region,
+    maps:{googleMaps}
   } = data[0];
 
   console.log(Object.values(languages));
   console.log(Object.values(currencies)[0].name);
   console.log(Object.values(currencies)[0].symbol);
 
-  countryDiv.innerHTML += `
+  countryDiv.innerHTML = `
     <div class="card mx-auto m-3 shadow-lg" style="width: 18rem;">
       <img src="${svg}" class="card-img-top" alt="...">
       <div class="card-body">
@@ -55,24 +100,22 @@ const renderCountries = (data) => {
           <i class="fas fa-lg fa-comments"></i> ${Object.values(languages)}
         </li>
         <li class="list-group-item">
-          <i class="fas fa-lg fa-money-bill-wave"></i> ${
-            Object.values(currencies)[0].name
-          },
-
-          ${Object.values(currencies)[0].symbol},
-
-        </li>
+          <i class="fas fa-lg fa-money-bill-wave"></i>
+          ${Object.values(currencies).map((item) => Object.values(item) + " ")}
+       </li>
       </ul>
-      <div class="card-body">
-        <a href="#" class="card-link">Card link</a>
-        <a href="#" class="card-link">Another link</a>
+ <div class="card-body text-center">
+        <a href="${googleMaps}" target="_blank" class="card-link btn btn-secondary">Google Maps</a>
+        
       </div>
     </div>
-
 
   `;
 };
 
+
+
+
+
 fetchCountryByName("turkey");
-fetchCountryByName("russia");
-fetchCountryByName("south africa");
+fetchCountryByAllName()
